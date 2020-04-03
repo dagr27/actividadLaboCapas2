09f63c54fbe9171b0abd0485f6d80ed982d4c397
dagr27 <00118717@uca.edu.sv>
2020-04-02 18:20:05 -0600

    Subiendo Actividad de labo

---
+++ src/main/java/com/example/demo/domain/Usuario.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain;
+
+public class Usuario {
+	private String nombre;
+	private String occupation;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getOcu() {
+		return occupation;
+	}
+	public void setNombre(String name) {
+		this.nombre = name;
+	}
+	public void setOcu(String ocu) {
+		this.occupation = ocu;
+	}
+
+}
